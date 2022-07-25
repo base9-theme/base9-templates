85ae07a6f5d071eff32e156a266eeeda2d7d1fa2
Jiaqi Li <lijiaqigreat@gmail.com>
2022-07-25 04:43:23 +0000

    only update readme if config readme present.

--- scripts/generate_readme.sh
+++ scripts/generate_readme.sh
@@ -1,7 +1,12 @@
 templates=`ls templates`
+temp_file=`mktemp base9.XXX`
 for name in $templates; do
   config="templates/$name/config.yml"
   if [ -f $config ]; then
-    mustache $config scripts/README.md.mustache > templates/$name/README.md
+    mustache $config scripts/README.md.mustache > $temp_file
+    if [ -s $temp_file ]; then
+      cp $temp_file templates/$name/README.md
+    fi
   fi
 done
+rm $temp_file
